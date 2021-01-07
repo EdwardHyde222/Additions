cb33a54e3f2e1e9b00f0f9a16ac88d25b87c6535
EddieHyde <xaskifs11@gmail.com>
2021-01-07 19:27:10 +0300

    new '.bat' file

---
+++ createJar.bat
@@ -0,0 +1,3 @@
+cd out/production/Additions
+C:\Users\User\.jdks\corretto-1.8.0_275\bin\jar.exe -cf ../../my.jar plugin.yml config.yml org ru
+copy out/my.jar "C:\Users\User\Desktop\Документы\Сервер Minecraft\Сервера\Основной сервер\Survival 1.16.3\plugins\"  
